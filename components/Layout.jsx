f38defb9880051338b9300060cee0be6ff698caa
Atik1000 <foysalkazi45@gmail.com>
2021-08-05 02:16:34 +0600

    product details added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,18 +1,35 @@
 import Head from 'next/head';
+import NextLink from 'next/link';
 import React from 'react';
-import {AppBar,Toolbar,Typography,Container } from '@material-ui/core';
+import {AppBar,Toolbar,Typography,Container, Link } from '@material-ui/core';
 import useStyles  from '../utils/styles';
 
-const Layout = ({ children }) => {
+const Layout = ({ children,title,description }) => {
     const classes = useStyles();
     return (
       <div>
         <Head>
-          <title>Amazon</title>
+          <title>{title ? `${title} - Next Amazona` : "Next Amazona"}</title>
+          {description && (
+            <meta name="description" content={description}></meta>
+          )}
         </Head>
         <AppBar position="static" className={classes.navbar}>
           <Toolbar>
-            <Typography>Amazon</Typography>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>Amazon</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
           </Toolbar>
         </AppBar>
         <Container className={classes.main}>{children}</Container>
