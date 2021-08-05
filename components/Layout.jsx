00ca477bbe8fa658e30be5441d3377c6c97473cc
Atik1000 <foysalkazi45@gmail.com>
2021-08-06 02:49:29 +0600

    switch button added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Head from 'next/head';
-import NextLink from 'next/link';
+import React, { useContext } from "react";
+import Head from "next/head";
+import NextLink from "next/link";
 import {
   AppBar,
   Toolbar,
@@ -10,41 +10,51 @@ import {
   createMuiTheme,
   ThemeProvider,
   CssBaseline,
-} from '@material-ui/core';
-import useStyles from '../utils/styles';
+  Switch,
+} from "@material-ui/core";
+import useStyles from "../utils/styles";
+import { Store } from "../utils/Store";
+import Cookies from "js-cookie";
 
 export default function Layout({ title, description, children }) {
+   const { state, dispatch } = useContext(Store);
+   const { darkMode } = state;
   const theme = createMuiTheme({
     typography: {
       h1: {
-        fontSize: '1.6rem',
+        fontSize: "1.6rem",
         fontWeight: 400,
-        margin: '1rem 0',
+        margin: "1rem 0",
       },
       h2: {
-        fontSize: '1.4rem',
+        fontSize: "1.4rem",
         fontWeight: 400,
-        margin: '1rem 0',
+        margin: "1rem 0",
       },
     },
     palette: {
-      type: 'light',
+      type: darkMode ? "dark" : "light",
       primary: {
-        main: '#f0c000',
+        main: "#f0c000",
       },
       secondary: {
-        main: '#208080',
+        main: "#208080",
       },
     },
   });
   const classes = useStyles();
+    const darkModeChangeHandler = () => {
+      dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+      const newDarkMode = !darkMode;
+      Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+    };
   return (
     <div>
       <Head>
-        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        <title>{title ? `${title} - Next Amazona` : "Next Amazona"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      
+
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar position="static" className={classes.navbar}>
@@ -56,6 +66,10 @@ export default function Layout({ title, description, children }) {
             </NextLink>
             <div className={classes.grow}></div>
             <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
               <NextLink href="/cart" passHref>
                 <Link>Cart</Link>
               </NextLink>
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 
 import '../styles/globals.css';
 import { useEffect } from 'react';
+import { StoreProvider } from "../utils/Store";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -10,7 +11,11 @@ function MyApp({ Component, pageProps }) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
-  return <Component {...pageProps} />;
+  return (
+       <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
 }
 
 export default MyApp;
